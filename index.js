04a6ec489c2dfde49ab300bda606c294f74156c9
Leirach <juandleon27@gmail.com>
2019-10-14 22:18:07 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,61 @@
+let response
+let videos = $("#videos")
+let q = ""
+let prevPage = ""
+let nextPage = ""
+
+function displayResults(items)
+{
+    videos.empty()
+    for (var i=0; i<items.length; i++){
+        let link = $("<a href=\"https://www.youtube.com/watch?v=" + items[i].id.videoId + "\"><div> </div><a/>")
+        let div  = link.find("div")
+        div.append($("<h3>" + items[i].snippet.title + "</h3>"))
+        div.append($("<img src=\"" + items[i].snippet.thumbnails.default.url + "\">"))
+        videos.append(link)
+    }
+
+}
+
+$(":button").click(function(event) 
+{
+    event.preventDefault()
+    var pageToken = ""
+
+    switch ($(this).attr("id")) {
+        case "searchButton":
+            q = $("#input").val()
+            break
+        case "next":
+            pageToken = nextPage
+            break
+        default:
+            pageToken = prevPage
+            break
+    }
+        
+
+    $.ajax({
+        url: "https://www.googleapis.com/youtube/v3/search",
+        method: "GET",
+        data: {
+                "key": "AIzaSyB8yvPbcXzmqZ9GY06II-1Fb3U4HqQm9u0",
+                "part": "id,snippet",
+                "q": q,
+                "maxResults": 10,
+                "pageToken": pageToken
+              }, //info sent to API
+        dataType: "json", //return type of the response
+        contentType: "application/json", //type of data in requests
+        success: function(responseJson) {
+            response = responseJson
+            nextPage = response.nextPageToken
+            prevPage = response.prevPageToken
+            displayResults(responseJson.items)
+        },
+        error: function(error) {
+            console.log("no videos found")
+        }
+    })
+})
+
